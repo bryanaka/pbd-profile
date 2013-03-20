1475a903da169d537b8f426ba1952bd2de01ff77
Bryan M. Robles <bmrobles@lbl.gov>
2013-03-20 11:23:51 -0700

    changed some minor things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,12 @@ private
 	helper_method :current_user
 	
 	def digest_shib_data
-		shibuser 						= ShibUser.new
-		shibuser.cn 				= request.env["HTTP_CN"]
-		shibuser.email 			= request.env["HTTP_MAIL"]
-		shibuser.eppn 			= request.env["HTTP_EPPN"]
+		shibuser 			= ShibUser.new
+		shibuser.cn 		= request.env["HTTP_CN"]
+		shibuser.email 		= request.env["HTTP_MAIL"]
+		shibuser.eppn 		= request.env["HTTP_EPPN"]
 		shibuser.given_name = request.env["HTTP_GIVENNAME"]
-		shibuser.sn 				= request.env["HTTP_SN"]
+		shibuser.sn 		= request.env["HTTP_SN"]
 
 		shibuser
 	end
