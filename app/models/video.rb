6214589fd8ab62cc65ecd84b5becafff89a0ffbe
Jerry Chung <jerrychung@lbl.gov>
2013-04-02 14:23:28 -0700

    Video form date fix

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,10 @@
 class Video < ActiveRecord::Base
-  attr_accessible :date, :description, :poster, :scientist, :title, :vid_code, :vid_link, :vid_type, :views
+  attr_accessible :description, :poster, :scientist, :title, :vid_code, :vid_link, :vid_type, :views
+
+  def date
+    if self[:date] != nil
+      newDate = Date.strptime self[:date], '%m/%d/%Y'
+    end
+  end
+
 end
